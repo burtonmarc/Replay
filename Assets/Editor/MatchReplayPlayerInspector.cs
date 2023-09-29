35cd4e3cbc7ea08198480c0f57d94569ffa0ae57
MarcVillegas <marcvillega90@gmail.com>
2023-09-29 08:42:36 +0200

    Added filters for MatchEventTypes and clear filters button

--- Assets/Editor/MatchReplayPlayerInspector.cs
+++ Assets/Editor/MatchReplayPlayerInspector.cs
@@ -1,26 +1,38 @@
 using System;
 using System.Collections.Generic;
 using System.Threading;
+using Unity.VisualScripting.YamlDotNet.Serialization.EventEmitters;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
+
+// TODO: Change how Agents are muted so they use the same system as muted MatchEventTypes
+// TODO: Add option to replay multiple matches with same scene at the same time
+
 
 [ExecuteInEditMode, CustomEditor(typeof(MatchReplayPlayer))]
 public class MatchReplayPlayerInspector : UnityEditor.Editor
 {
     private MatchReplayPlayer matchReplayPlayer;
-    private bool displayLevel;
-    private MatchData matchData;
+    private static bool displayLevel;
+    private static MatchData matchData;
+    private static List<MatchEventType> eventTypesMuted;
 
     private void Awake()
     {
         matchReplayPlayer = target as MatchReplayPlayer;
-        SceneView.beforeSceneGui += TryPaintScene;
+
+        if (eventTypesMuted == null)
+        {
+            eventTypesMuted = new List<MatchEventType>();
+        }
+        
+        SceneView.beforeSceneGui += TryDrawScene;
     }
 
     private void OnDestroy()
     {
-        SceneView.beforeSceneGui -= TryPaintScene;
+        SceneView.beforeSceneGui -= TryDrawScene;
     }
 
     public override void OnInspectorGUI()
@@ -53,11 +65,11 @@ public class MatchReplayPlayerInspector : UnityEditor.Editor
 
     private void OnSceneGUI()
     {
-        TryPaintScene(null);
-        PaintUI();
+        TryDrawScene(null);
+        DrawUI();
     }
     
-    private void TryPaintScene(SceneView obj)
+    private void TryDrawScene(SceneView obj)
     {
         if (displayLevel && matchData != null)
         {
@@ -67,91 +79,182 @@ public class MatchReplayPlayerInspector : UnityEditor.Editor
             {
                 if(matchAgentData.Mute) continue;
                 
-                // Draw Position Events
-                Handles.color = Color.green;
-                var skippedFirstPositionEvent = false;
-                for (var index = 0; index < matchAgentData.PositionEvents.Count; index++)
-                {
-                    var positionEvent = matchAgentData.PositionEvents[index];
-                    
-                    if(!TimeStampInsideRange(maxReplayTime, positionEvent.TimeStamp)) continue;
+                DrawPositionEvents(matchAgentData, maxReplayTime);
 
-                    if (!skippedFirstPositionEvent)
-                    {
-                        skippedFirstPositionEvent = true;
-                        continue;
-                    }
-                    
-                    var previousPositionEvent = matchAgentData.PositionEvents[index - 1];
-                    
-                    if (matchAgentData.EliminationEvents.Exists(ee => 
-                            previousPositionEvent.TimeStamp < ee.TimeStamp &&
-                            positionEvent.TimeStamp > ee.TimeStamp)) continue;
-                    
-                        
-                    Handles.DrawLine(previousPositionEvent.Pos / matchData.DecimalPrecision,
-                        positionEvent.Pos / matchData.DecimalPrecision, 3.0f);
-                }
-
-                // Draw Elimination Events
-                Handles.color = Color.red;
-                for (var index = 0; index < matchAgentData.EliminationEvents.Count; index++)
-                {
-                    var eliminationEvent = matchAgentData.EliminationEvents[index];
-                    
-                    if(!TimeStampInsideRange(maxReplayTime, eliminationEvent.TimeStamp)) continue;
-                    
-                    Handles.DrawWireCube(eliminationEvent.Pos / matchData.DecimalPrecision, new Vector3(0.2f, 1f, 0.2f));
-                    Handles.DrawWireCube(eliminationEvent.Pos / matchData.DecimalPrecision + new Vector3(0f, 0.2f, 0f), new Vector3(0.6f, 0.2f, 0.2f));
-                }
+                DrawEliminationEvents(matchAgentData, maxReplayTime);
             }
             SceneView.RepaintAll();
         }
     }
 
-    private void PaintUI()
+    private void DrawPositionEvents(AgentData matchAgentData, float maxReplayTime)
+    {
+        if (eventTypesMuted != null && eventTypesMuted.Contains(MatchEventType.Position)) return;
+        
+        // Draw Position Events
+        Handles.color = Color.green;
+        var skippedFirstPositionEvent = false;
+        for (var index = 0; index < matchAgentData.PositionEvents.Count; index++)
+        {
+            var positionEvent = matchAgentData.PositionEvents[index];
+
+            if (!TimeStampInsideRange(maxReplayTime, positionEvent.TimeStamp)) continue;
+
+            if (!skippedFirstPositionEvent)
+            {
+                skippedFirstPositionEvent = true;
+                continue;
+            }
+
+            var previousPositionEvent = matchAgentData.PositionEvents[index - 1];
+
+            if (matchAgentData.EliminationEvents.Exists(ee =>
+                    previousPositionEvent.TimeStamp < ee.TimeStamp &&
+                    positionEvent.TimeStamp > ee.TimeStamp)) continue;
+
+
+            Handles.DrawLine(previousPositionEvent.Pos / matchData.DecimalPrecision,
+                positionEvent.Pos / matchData.DecimalPrecision, 3.0f);
+        }
+    }
+    
+    private void DrawEliminationEvents(AgentData matchAgentData, float maxReplayTime)
+    {
+        if (eventTypesMuted != null && eventTypesMuted.Contains(MatchEventType.Elimination)) return;
+        
+        // Draw Elimination Events
+        Handles.color = Color.red;
+        for (var index = 0; index < matchAgentData.EliminationEvents.Count; index++)
+        {
+            var eliminationEvent = matchAgentData.EliminationEvents[index];
+
+            if (!TimeStampInsideRange(maxReplayTime, eliminationEvent.TimeStamp)) continue;
+
+            Handles.DrawWireCube(eliminationEvent.Pos / matchData.DecimalPrecision, new Vector3(0.2f, 1f, 0.2f));
+            Handles.DrawWireCube(eliminationEvent.Pos / matchData.DecimalPrecision + new Vector3(0f, 0.2f, 0f),
+                new Vector3(0.6f, 0.2f, 0.2f));
+        }
+    }
+
+    private void DrawUI()
     {
         if (displayLevel && matchData != null)
         {
             Handles.BeginGUI();
-            GUILayout.BeginArea(new Rect(20, 20, 100, 100));
-
+            
+            
+            GUILayout.BeginArea(new Rect(20, 20, 200, 500));
             var rect = EditorGUILayout.BeginVertical();
-            
             GUI.Box(rect, GUIContent.none);
-
-            GUILayout.Label("Agents");
-
-            EditorGUILayout.BeginVertical();
             
-            foreach (var matchAgentData in matchData.AgentsData)
-            {
-                EditorGUILayout.BeginHorizontal();
-                
-                GUILayout.Label(matchAgentData.AgentName);
-                matchAgentData.Mute = GUILayout.Toggle(matchAgentData.Mute, "mute");
-
-                var solo = false;
-                solo = GUILayout.Toggle(solo, "solo");
-
-                if (solo)
-                {
-                    matchAgentData.Mute = false;
-                    foreach (var mAD in matchData.AgentsData)
-                    {
-                        if (mAD != matchAgentData)
-                        {
-                            mAD.Mute = true;
-                        }
-                    }
-                }
-                
-                EditorGUILayout.EndHorizontal();
-            }
+            DrawAgentsUI();
+            DrawEventsUI();
+            DrawClearFiltersUI();
             
             EditorGUILayout.EndVertical();
             GUILayout.EndArea();
+            
             Handles.EndGUI();
+        }
+    }
+
+    private void DrawAgentsUI()
+    {
+        GUILayout.Label("Agents");
+
+        foreach (var matchAgentData in matchData.AgentsData)
+        {
+            EditorGUILayout.BeginHorizontal();
+                
+            GUILayout.Label(matchAgentData.AgentName);
+            matchAgentData.Mute = GUILayout.Toggle(matchAgentData.Mute, "MUTE");
+
+            var solo = GUILayout.Button("SOLO");
+
+            if (solo)
+            {
+                matchAgentData.Mute = false;
+                foreach (var mAD in matchData.AgentsData)
+                {
+                    if (mAD != matchAgentData)
+                    {
+                        mAD.Mute = true;
+                    }
+                }
+            }
+                
+            EditorGUILayout.EndHorizontal();
+        }
+
+        Separator();
+    }
+
+    private void DrawEventsUI()
+    {
+        GUILayout.Label("Event Types");
+
+        var matchEventTypes = Enum.GetNames (typeof(MatchEventType));
+
+        if (eventTypesMuted == null)
+        {
+            eventTypesMuted = new List<MatchEventType>();
+        }
+
+        for (var index = 0; index < matchEventTypes.Length; index++)
+        {
+            EditorGUILayout.BeginHorizontal();
+            
+            var matchEventType = matchEventTypes[index];
+            GUILayout.Label(matchEventType);
+
+            var muted = GUILayout.Toggle(eventTypesMuted.Exists(eventType => eventType.ToString() == matchEventType), "MUTE");
+
+            if (muted)
+            {
+                if (!eventTypesMuted.Contains((MatchEventType)index))
+                {
+                    eventTypesMuted.Add((MatchEventType)index);
+                }
+            }
+            else
+            {
+                if (eventTypesMuted.Contains((MatchEventType)index))
+                {
+                    eventTypesMuted.Remove((MatchEventType)index);
+                }
+            }
+            
+            var solo = GUILayout.Button("SOLO");
+
+            if (solo)
+            {
+                eventTypesMuted.Clear();
+                foreach (var met in matchEventTypes)
+                {
+                    if (met != matchEventType)
+                    {
+                        var type = (MatchEventType)Enum.Parse( typeof(MatchEventType), met );
+                        eventTypesMuted.Add(type);
+                    }
+                }
+            }
+
+            EditorGUILayout.EndHorizontal();
+        }
+        
+        Separator();
+    }
+
+    private void DrawClearFiltersUI()
+    {
+        if (GUILayout.Button("CLEAR FILTERS"))
+        {
+            foreach (var mAD in matchData.AgentsData)
+            {
+                mAD.Mute = false;
+            }
+            
+            eventTypesMuted.Clear();
         }
     }
 
@@ -163,5 +266,14 @@ public class MatchReplayPlayerInspector : UnityEditor.Editor
             matchTime * matchReplayPlayer.replayEndPercentage * 0.01f)
             return false;
         return true;
+    }
+
+    private void Separator()
+    {
+        GUILayout.BeginHorizontal();
+        GUILayout.FlexibleSpace();
+        GUILayout.Label("----------------");
+        GUILayout.FlexibleSpace();
+        GUILayout.EndHorizontal();
     }
 }
